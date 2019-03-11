d0e1be0791802be2df4d6ea512c73ac45ae3d9af
KentTain <tianchangjun@outlook.com>
2019-03-11 15:37:47 +0800

    dockerfile

--- TestWeb/build.sh
+++ TestWeb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "----$1------$env------"
 echo "dotnet run --environment=$ASPNETCORE_ENVIRONMENT with TestWeb.dll"
-dotnet run --environment="$ASPNETCORE_ENVIRONMENT" TestWeb.dll
+dotnet run -p 5000:5000 --environment="$ASPNETCORE_ENVIRONMENT" TestWeb.dll
